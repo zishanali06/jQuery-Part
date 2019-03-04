9f172dc025d5abdba009281c5ff49df3cad04e84
Zishan Ali <zishanali06@gmail.com>
2019-03-04 12:08:41 -0600

    step 5

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ $(document).ready(function(){
     let colors = ['red', 'blue', 'green', 'yellow', 'aqua'];
     para.click(function(){
         para.css('color', colors[Math.floor(Math.random() * 5)]);
+    });
+
+    let button3 = $('<button class="button3">Click me For Span</button>');
+    $('body').append(button3);
+    let newdiv = $('<div></div>');
+    $('body').append(newdiv);
+
+    button3.click(function(){
+        newdiv.append('<span>Zishan</span>')
     })
+
 });
 
 $('.button2').click(function(){
